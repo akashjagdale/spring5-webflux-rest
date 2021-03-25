2b4c0fab38facac327332a885b06c3b20950917d
akashjagdale <adjagdale25@gmail.com>
2021-03-25 23:16:19 +0530

    Create Category with POST

--- src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -4,8 +4,10 @@ import guru.springframework.spring5webfluxrest.domain.Category;
 import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -19,7 +21,7 @@ public class CategoryControllerTest {
     CategoryController categoryController;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         categoryRepository = Mockito.mock(CategoryRepository.class);
         categoryController = new CategoryController(categoryRepository);
         webTestClient = WebTestClient.bindToController(categoryController).build();
@@ -47,5 +49,21 @@ public class CategoryControllerTest {
                 .uri("/api/v1/categories/someId")
                 .exchange()
                 .expectBody(Category.class);
+    }
+
+    @Test
+    public void testCreateCategory() {
+        BDDMockito.given(categoryRepository.saveAll(ArgumentMatchers.any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> categoryToSave = Mono.just(Category.builder().description("Some Desc").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(categoryToSave, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+
     }
 }
